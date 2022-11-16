625eba67ed11a40bcaf2f87f312823c9644ca897
elementodjinn <44180850+elementodjinn@users.noreply.github.com>
2022-11-16 15:48:15 -0800

    Time to merge

--- Assets/Scripts/StoryManager.cs
+++ Assets/Scripts/StoryManager.cs
@@ -9,6 +9,11 @@ public class StoryManager : MonoBehaviour
     Flowchart flowchart;
     bool companionTalk = false;
     int eventCounter = 0;
+    string baseBlockName = "";
+    string nextLine = "";
+    [SerializeField]
+    List<string> companionText;
+    List<string> usedText = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
@@ -25,10 +30,25 @@ public class StoryManager : MonoBehaviour
     {
         companionTalk = !companionTalk;
         flowchart.SetBooleanVariable("companionTalk", companionTalk);
+        flowchart.ExecuteBlock(baseBlockName);
     }
 
     void ChangeStoryProgress()
     {
 
+    }
+
+    void GenerateCompanionText()
+    {
+        if (companionText.Count == 0)
+        {
+            companionText = usedText;
+            usedText = new List<string>();
+        }
+        int index = Random.Range(0, companionText.Count);
+        nextLine = companionText[index];
+        flowchart.SetStringVariable("nextLine", nextLine);
+        usedText.Add(nextLine);
+        companionText.Remove(nextLine);
     }
 }
